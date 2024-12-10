1aa250a5b7f03afc49f57a88c3e97095364acac6
Maciej Bogusławski <01200725@pw.edu.pl>
2024-12-10 21:49:31 +0100

    Update p1.asm

--- p1.asm
+++ p1.asm
@@ -1,11 +1,10 @@
 .data 10
-x: 0     ; Komórka, do której zapisujemy wynik (0 lub 1)
+x: 5     ; Komórka, do której zapisujemy wynik (0 lub 1)
 y: -1    ; Komórka przechowująca liczbę do sprawdzenia (przykładowa wartość)
 
-.code 200
+.code 100
 START:    CPA y          ; 1aaaa - Skopiuj wartość z adresu y do akumulatora
            BRN NEGATIVE  ; 7aaaa - Skocz do NEGATIVE, jeśli wartość w akumulatorze jest ujemna
-           STO x         ; 2aaaa - Zapisz wartość 0 (domyślną w akumulatorze) do komórki x
            HLT           ; 00000 - Zatrzymaj program
 NEGATIVE: CPA 1          ; 92101 - Wczytaj wartość 1 do akumulatora
            STO x         ; 2aaaa - Zapisz wartość 1 do komórki x
